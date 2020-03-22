afd396d5acd7e2672aa0d6a6e956c62ad5be5804
Wangchong Zhou <fffonion@gmail.com>
2020-03-23 02:16:07 +0800

    Update xeHentaiHelper.user.js

--- xeHentaiHelper.user.js
+++ xeHentaiHelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        xeHentai Helper
-// @version     0.13
+// @version     0.20
 // @description Become a hentai
 // @namespace 	https://yooooo.us
 // @updateURL 	https://dl.yooooo.us/userscripts/xeHentaiHelper.user.js
@@ -10,11 +10,33 @@
 // @include     http*://*e-hentai.org/?*
 // @include     http*://*e-hentai.org/g/*
 // @include     http*://*e-hentai.org/tag/*
+// @include     http*://*e-hentai.org/uploader/*
 // @include     http*://exhentai.org/
 // @include     http*://exhentai.org/#
 // @include     http*://exhentai.org/?*
 // @include     http*://exhentai.org/g/*
 // @include     http*://exhentai.org/tag/*
+// @include     http*://exhentai.org/uploader/*
+// @include     http*://*e-hentai.org/non-h
+// @include     http*://*e-hentai.org/misc
+// @include     http*://*e-hentai.org/doujinshi
+// @include     http*://*e-hentai.org/western
+// @include     http*://*e-hentai.org/manga
+// @include     http*://*e-hentai.org/imageset
+// @include     http*://*e-hentai.org/artistcg
+// @include     http*://*e-hentai.org/cosplay
+// @include     http*://*e-hentai.org/gamecg
+// @include     http*://*e-hentai.org/asianporn
+// @include     http*://exhentai.org/non-h
+// @include     http*://exhentai.org/misc
+// @include     http*://exhentai.org/doujinshi
+// @include     http*://exhentai.org/western
+// @include     http*://exhentai.org/manga
+// @include     http*://exhentai.org/imageset
+// @include     http*://exhentai.org/artistcg
+// @include     http*://exhentai.org/cosplay
+// @include     http*://exhentai.org/gamecg
+// @include     http*://exhentai.org/asianporn
 // @license     GNU General Public License (GPL)
 // @run-at      document-end
 // @grant none
@@ -22,12 +44,8 @@
 
 // ==== ARIA2 class taken from Binux's ThunderLixianExported === //
 
-(function (){
-    var xeh_endpoints = {
-        "default":"http://127.0.0.1:8010/jsonrpc",
-    };
-
-    var JSONRPC = (function() {
+(function () {
+    var JSONRPC = (function () {
         var jsonrpc_version = '2.0';
 
         function get_auth(url) {
@@ -47,85 +65,312 @@
             if (params) request_obj.params = params;
             if (auth && auth.indexOf('token:') == 0) params.unshift(auth);
 
-            xhr.open("POST", jsonrpc_path+"?tm="+(new Date()).getTime().toString(), true);
+            xhr.open("POST", jsonrpc_path + "?tm=" + (new Date()).getTime().toString(), true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
             if (auth && auth.indexOf('token:') != 0) {
-                xhr.setRequestHeader("Authorization", "Basic "+btoa(auth));
+                xhr.setRequestHeader("Authorization", "Basic " + btoa(auth));
             }
             xhr.send(JSON.stringify(request_obj));
         };
 
-        return function(jsonrpc_path) {
+        return function (jsonrpc_path) {
             this.jsonrpc_path = jsonrpc_path;
             this.addTask = function (uri, options) {
-                request(this.jsonrpc_path, 'xeH.addTask', [[uri, ], options]);
+                request(this.jsonrpc_path, 'xeH.addTask', [
+                    [uri, ], options
+                ]);
             };
             this.setCookie = function (cookie) {
-                request(this.jsonrpc_path, 'xeH.setCookie', [[cookie, ], {}]);
+                request(this.jsonrpc_path, 'xeH.setCookie', [
+                    [cookie, ], {}
+                ]);
             };
             return this;
         };
     })();
-    XEH = {};
-    (function(XEH){
-        var jr = JSONRPC(xeh_endpoints['default']);
+
+    // dom helper functions
+    function newWrapperDiv(label, dom) {
+        var grp = document.createElement("div");
+        grp.innerHTML = label + "：";
+        grp.appendChild(dom);
+        return grp;
+    }
+
+    function newInput() {
+        var input = document.createElement("input");
+        input.type = "text";
+        input.size = 50;
+        input.style = "margin-bottom: 5px"
+        return input;
+    }
+
+    function newButton(label, style, f) {
+        var btn = document.createElement("div");
+        btn.innerHTML = "<a href='#'>" + label + "</a>";
+        btn.style = "position: absolute; " + (style || "");
+        btn.onclick = f;
+        btn.className = "gt";
+        return btn;
+    }
+
+    XEH = {
+        config_keys: ["host", "port", "token", "name"],
+    };
+    (function (XEH) {
+        // JSONRPC client, will be initlized in the config area
+        var jr;
         var gd5 = document.getElementById("gd5");
-        if(gd5){ // gallery page
+        if (gd5) { // gallery page
             var p = document.createElement("p");
             p.innerHTML = '<img src="https://ehgt.org/g/mr.gif"> <a id="xeh_addtask" href="#">添加到xeHentai</a>';
             p.className = "g2";
-            p.onclick = function(){
-                if (document.cookie.indexOf("ipb_pass_hash") != -1 && document.cookie.indexOf("ipb_member_id") != -1){
+            p.onclick = function () {
+                if (document.cookie.indexOf("ipb_pass_hash") != -1 && document.cookie.indexOf("ipb_member_id") != -1) {
                     jr.setCookie(document.cookie);
                 }
-                jr.addTask(location.protocol+'//'+location.host+location.pathname, {});
+                jr.addTask(location.protocol + '//' + location.host + location.pathname, {});
             };
-            gd5.childNodes[gd5.childNodes.length-1].className = "g2";
+            gd5.childNodes[gd5.childNodes.length - 1].className = "g2";
             gd5.appendChild(p);
         }
         var glnames = document.getElementsByClassName("glname");
-        if(glnames && glnames.length){ // index page
-            var inp_onclick = function(e){ e.stopPropagation() };
+        if (glnames && glnames.length) { // index page
+            function saveInputState() {
+                if (!localStorage) return;
+                var checked = {};
+                for (var i = 0; i < allinps.length; ++i) {
+                    if (allinps[i].checked) {
+                        checked[allinps[i].value] = 1;
+                    }
+                }
+                localStorage.setItem("xeh_checked", JSON.stringify(checked));
+            }
+
+            function loadInputState() {
+                if (!localStorage) return;
+                var checked = JSON.parse(localStorage.getItem("xeh_checked"));
+                if (!checked) return;
+                for (var i = 0; i < allinps.length; ++i) {
+                    if (checked[allinps[i].value] === 1) {
+                        allinps[i].checked = true;
+                    }
+                }
+            }
+            var inp_onclick = function (e) {
+                e.stopPropagation();
+                saveInputState();
+            };
             var allinps = [];
             for (var i = 0; i < glnames.length; ++i) {
                 var glname = glnames[i];
                 var ip = document.createElement("input");
                 ip.type = "checkbox";
-                ip.style= "float:left;font-size:20px;top:0;";
-                console.log(glname);
-                ip.value = decodeURIComponent(glname.childNodes[0].href || glname.childNodes[0].childNodes[0].href);
+                ip.style = "float:left;font-size:20px;top:0;";
+                var href;
+                var href_dom = glname;
+                for (var j = 0; j < 3; j++) {
+                    href = href_dom.innerHTML.match(/\/g\/\d+\/[a-f0-9]+\//);
+                    if (href) break;
+                    href_dom = href_dom.parentNode;
+                }
+                ip.value = decodeURIComponent(location.protocol + "//" + location.hostname + href[0]);
                 ip.onclick = inp_onclick;
-                glname.childNodes[0].insertBefore(ip, glname.childNodes[0].childNodes[0]);
+                var doms = href_dom.childNodes;
+                for (var j = 0; j < doms.length; j++) {
+                    if (doms[j].tagName === "A") {
+                        doms[j].insertBefore(ip, doms[j].childNodes[0]);
+                    }
+                }
                 allinps.push(ip);
             }
             var titlebar = document.getElementsByClassName("itg")[0].childNodes[0].childNodes[0];
             var titleInnerHTML = '<input type="checkbox" id="xeh_toogle" style="margin-left:9px;top:0;">反选</input>' +
-                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a id="xeh_export" href="#">导出选中项到xeHentai</a>';
-            if (glnames[0].className.search(/gl\d[te]/) != -1){ // extended or thumbail mode
+                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a id="xeh_clear" href="javascript:void(0)">清空</a>' +
+                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a id="xeh_export" href="javascript:void(0)">导出选中项到xeHentai</a>';
+            if (glnames[0].className.search(/gl\d[te]/) != -1) { // extended or thumbail mode
                 var td = document.createElement("td");
                 td.innerHTML = titleInnerHTML;
                 td.style = "width: auto;position: absolute;left: 10px;font-size: 12px;";
                 var tr = document.getElementsByClassName("ptt")[0].childNodes[0].childNodes[0]
                 tr.insertBefore(td, tr.childNodes[0]);
             } else {
-                titlebar.childNodes[2].innerHTML = titleInnerHTML;
+                for (var j = 0; j < titlebar.childNodes.length; j++) {
+                    if (titlebar.childNodes[j].innerHTML.search(/title/i) !== -1) {
+                        titlebar.childNodes[j].innerHTML = titleInnerHTML;
+                    }
+                }
             }
-            document.getElementById("xeh_toogle").onclick = function(s){
+
+            loadInputState();
+
+            document.getElementById("xeh_toogle").onclick = function (s) {
                 for (var i = 0; i < allinps.length; ++i) {
                     allinps[i].checked = !allinps[i].checked;
                 }
+                saveInputState();
             };
-            document.getElementById("xeh_export").onclick = function(s){
-                if (document.cookie.indexOf("ipb_pass_hash") != -1 && document.cookie.indexOf("ipb_member_id") != -1){
+            document.getElementById("xeh_clear").onclick = function (s) {
+                for (var i = 0; i < allinps.length; ++i) {
+                    allinps[i].checked = false;
+                }
+                saveInputState();
+            };
+            document.getElementById("xeh_export").onclick = function (s) {
+                if (document.cookie.indexOf("ipb_pass_hash") != -1 && document.cookie.indexOf("ipb_member_id") != -1) {
                     jr.setCookie(document.cookie);
                 }
                 for (var i = 0; i < allinps.length; ++i) {
-                    if(allinps[i].checked){
+                    if (allinps[i].checked) {
                         jr.addTask(allinps[i].value, {});
                     }
                 }
             };
         }
+
+        // config
+        var titleBar = document.getElementById("nb");
+        if (titleBar) {
+            var div = document.createElement("div");
+            div.innerHTML = '<div><a>xeHentai</a></div>';
+            div.onclick = function () {
+                configBox.style.display = "block";
+            };
+            titleBar.appendChild(div);
+
+            // the config box
+            var configBox = document.createElement("div");
+            configBox.className = "gm"
+            configBox.innerHTML = '<h1 id="gn" style="text-align: center;">xeHentai 配置</h1>';
+            configBox.style = "height: 300px; position: absolute; top: 200px; z-index: 999; left: 50%; margin-left: -250px; min-width: 320px;";
+            configBox.style.display = "none";
+            document.body.appendChild(configBox)
+
+            /****************** starts top buttons ************************/
+            var closeBtn = newButton("关闭", "right: 5px; top: 5px;", function () {
+                configBox.style.display = "none";
+            });
+            configBox.appendChild(closeBtn);
+            /****************** ends top buttons ************************/
+
+            /****************** starts input areas ************************/
+            var controlsGrp = document.createElement("div");
+            controlsGrp.style = "padding: 10px;"
+
+            XEH.configs = undefined;
+            var inputs = {};
+
+            function saveConfigSet() {
+                if (!localStorage) return;
+                localStorage.setItem("xeh_configs", JSON.stringify(XEH.configs));
+                localStorage.setItem("xeh_config_idx", configSet.selectedIndex);
+            }
+
+            function loadConfigSet(i) {
+                if (XEH.configs === undefined) {
+                    if (!localStorage) return;
+                    XEH.configs = JSON.parse(localStorage.getItem("xeh_configs")) || [{
+                        "host": "localhost",
+                        "port": 8090,
+                        "name": "<默认>"
+                    }];
+                }
+                if (i === undefined) {
+                    i = parseInt(localStorage.getItem("xeh_config_idx")) || 0;
+                }
+
+                XEH.configs[i].name = XEH.configs[i].name || "<未命名>";
+                for (var j = 0; j < XEH.config_keys.length; j++) {
+                    var k = XEH.config_keys[j];
+                    inputs[k].value = XEH.configs[i][k];
+                }
+
+                return i;
+            }
+            function initJSONRPC(i) {
+                var cfg = XEH.configs[i]
+                jr = JSONRPC("http://" + (cfg.token ? ("token:" + cfg.token + "@") : "") +
+                    cfg.host + ":" + cfg.port + "/jsonrpc");
+            }
+
+            var hasNewConfigUnsaved = false;
+
+            // add <select> first, its options will be filled later
+            var configSet = document.createElement("select");
+            configSet.onchange = function (i) {
+                if (hasNewConfigUnsaved) {
+                    configSet.remove(configSet.options.length - 1)
+                    hasNewConfigUnsaved = false;
+                }
+                var i = configSet.selectedIndex
+                loadConfigSet(i)
+                initJSONRPC(i)
+            }
+            controlsGrp.appendChild(newWrapperDiv("当前配置", configSet));
+
+            configBox.appendChild(controlsGrp);
+
+            var __input_labels = ["地址", "端口", "密钥", "名称"];
+            for (var i = 0; i < XEH.config_keys.length; i++) {
+                var k = XEH.config_keys[i];
+                inputs[k] = newInput();
+                controlsGrp.appendChild(newWrapperDiv(__input_labels[i], inputs[k]));
+            }
+            inputs.port.size = 6;
+
+            // load input boxes etc.
+            var shouldSelectedIdx = loadConfigSet();
+
+            for (var i = 0; i < XEH.configs.length; i++) {
+                var c = document.createElement("option");
+                c.text = XEH.configs[i].name;
+                configSet.options.add(c)
+            }
+            configSet.selectedIndex = shouldSelectedIdx;
+            initJSONRPC(shouldSelectedIdx)
+            /****************** ends input areas ************************/
+
+            /****************** starts bottom buttons ************************/
+            var addBtn = newButton("新建", "left: 20px; bottom: 5px;", function () {
+                for (var i = 0; i < XEH.config_keys.length; i++) {
+                    var k = XEH.config_keys[i];
+                    inputs[k].value = "";
+                }
+                var c = document.createElement("option");
+                c.text = "<新配置>";
+                configSet.options.add(c);
+                configSet.selectedIndex = configSet.options.length - 1;
+                hasNewConfigUnsaved = true;
+            });
+            configBox.appendChild(addBtn);
+
+            var ojbkBtn = newButton("保存", "left: 100px; bottom: 5px;", function () {
+                var idx = configSet.selectedIndex;
+                var cfg;
+                if (hasNewConfigUnsaved) {
+                    hasNewConfigUnsaved = false
+                    cfg = {}
+                    XEH.configs.push(cfg)
+                } else {
+                    cfg = XEH.configs[idx]
+                }
+                cfg.token = cfg.token || ""
+                for (var i = 0; i < XEH.config_keys.length; i++) {
+                    var k = XEH.config_keys[i];
+                    cfg[k] = inputs[k].value;
+                }
+                configSet.options.item(idx).text = cfg.name || (cfg.host + ":" + cfg.port);
+                saveConfigSet();
+                initJSONRPC(idx);
+            });
+            configBox.appendChild(ojbkBtn);
+
+            var resetBtn = newButton("重置", "left: 150px; bottom: 5px;", function () {
+                loadConfigSet(configSet.selectedIndex)
+            });
+            configBox.appendChild(resetBtn);
+
+            /****************** ends bottom buttons ************************/
+        }
     })(XEH);
 })();
-
